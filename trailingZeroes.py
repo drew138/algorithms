f0cf52e4af05fcc8f550dbf786f3de9c5b08e71d
drew <andresalazar138@gmail.com>
2020-11-29 23:42:33 -0500

    tweaked factorial trailing zeroes to run in log5 time and constant space

--- trailingZeroes.py
+++ trailingZeroes.py
@@ -1,14 +1,12 @@
 # problem: https://leetcode.com/problems/factorial-trailing-zeroes/submissions/
-# Runtime: 124 ms, faster than 33.28% of Python3 online submissions for Factorial Trailing Zeroes.
-# Memory Usage: 14 MB, less than 96.37% of Python3 online submissions for Factorial Trailing Zeroes.
+# Runtime: 24 ms, faster than 95.76% of Python3 online submissions for Factorial Trailing Zeroes.
+# Memory Usage: 14 MB, less than 81.88% of Python3 online submissions for Factorial Trailing Zeroes.
 
 
 class Solution:
     def trailingZeroes(self, n: int) -> int:
         answer = 0
-        for i in range(0, n + 1, 5):
-            x = i
-            while (x % 5 == 0) and x != 0:
-                x /= 5
-                answer += 1
+        while n:
+            answer += n//5
+            n //= 5
         return answer
