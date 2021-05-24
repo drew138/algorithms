b26da46f032547c285fa0c53c10cc16d58fd073c
drew <andresalazar138@gmail.com>
2021-05-24 16:13:38 -0500

    finished toLowerCase.py problem.

---
+++ toLowerCase.py
@@ -0,0 +1,8 @@
+# problem: https://leetcode.com/problems/to-lower-case/submissions/
+# Runtime: 24 ms, faster than 92.06% of Python3 online submissions for To Lower Case.
+# Memory Usage: 14 MB, less than 86.74% of Python3 online submissions for To Lower Case.
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
