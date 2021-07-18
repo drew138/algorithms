4dddab4fc82a9aefaa3b9d83a9db0cfbb732d441
drew <andresalazar138@gmail.com>
2021-07-18 11:50:13 -0500

    finished reverseNodesInKGroup.py problem.

--- reverseNodesInKGroup.py
+++ reverseNodesInKGroup.py
@@ -4,10 +4,11 @@
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 
 class Solution:
     def reverseInnerList(self, node, k):
