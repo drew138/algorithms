0835b6702d1351d5820a80509c414f9adb1c10ec
drew <andresalazar138@gmail.com>
2022-03-30 14:11:46 -0500

    finished searchA2DMatrix.cpp problem.

---
+++ searchA2DMatrix.cpp
@@ -0,0 +1,32 @@
+// problem: https://leetcode.com/problems/search-a-2d-matrix/
+// Runtime: 4 ms, faster than 77.73% of C++ online submissions for Search a 2D Matrix.
+// Memory Usage: 9.5 MB, less than 52.00% of C++ online submissions for Search a 2D Matrix.
+
+#include <bits/stdc++.h>
+
+#define D(x) cout << #x << " = " << x << endl;
+#define ios ios_base::sync_with_stdio(0), cin.tie(0);
+#define forn(i, n) for (int i = 0; i < (int)n; ++i)
+#define all(v) v.begin(), v.end()
+#define allgreater(v) v.begin(), v.end(), greater<int>()
+#define formap(map) for (const auto &[key, value] : map)
+#define ms(ar, val) memset(ar, val, sizeof ar)
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        int i = 0, j = m - 1;
+        while (i < n && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] > target) j--;
+            else i++;
+        }
+        return false;
+    }
+};
+
