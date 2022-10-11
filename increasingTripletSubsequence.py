d61a7550b71899d986bf14c6c8ae6986bb34af17
drew <58788781+drew138@users.noreply.github.com>
2022-10-11 18:33:22 -0500

    finished increasingTripletSubsequence.py problem.

--- increasingTripletSubsequence.py
+++ increasingTripletSubsequence.py
@@ -1,24 +1,36 @@
 # problem: https://leetcode.com/problems/increasing-triplet-subsequence/
-# Runtime: 56 ms, faster than 46.89% of Python3 online submissions for Increasing Triplet Subsequence.
-# Memory Usage: 14.9 MB, less than 7.12% of Python3 online submissions for Increasing Triplet Subsequence.
+# Runtime: 588 ms, faster than 93.05% of Python3 online submissions for Increasing Triplet Subsequence.
+# Memory Usage: 24.7 MB, less than 49.03% of Python3 online submissions for Increasing Triplet Subsequence.from typing import List
 
+import bisect
+import math
 from typing import List
 
 
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        from collections import deque
-        stack = deque()
-        maxLen = 0
+        first, second, third = math.inf, math.inf, math.inf
         for num in nums:
-            while stack and stack[-1] >= num:
-                stack.pop()
-            stack.append(num)
-            maxLen = max(maxLen, len(stack))
-        incr = 0
-        cur = nums[0]
-        for num in nums:
-            if num > cur:
-                incr += 1
-                cur = num
-        return (maxLen >= 3) or (incr >= 2)
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            elif num <= third:
+                third = num
+
+        return first < second < third and (third != math.inf)
+
+
+# Alternate solution O(nlogn)
+# class Solution:
+#     def increasingTriplet(self, nums: List[int]) -> bool:
+#
+#         tmp = [nums[0]]
+#         for num in nums:
+#             if num > tmp[-1]:
+#                 tmp.append(num)
+#             else:
+#                 index = bisect.bisect_left(tmp, num)
+#                 tmp[index] = num
+#
+#         return len(tmp) >= 3
